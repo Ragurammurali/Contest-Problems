ead67579724b19f08db8b01d3bd3d164789296aa
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 17:08:40 +0530

    Code for the Problem Christmas Greetings
    This is the Code for the Problem: Christmas Greetings

---
+++ Christmas Greetings.py
@@ -0,0 +1,10 @@
+- Link: https://www.codechef.com/problems/CHRISTGREET 
+- Contest: Starters 114
+- Difficulty: 205
+
+Code:
+X=int(input())
+if(X==25):
+    print("CHRISTMAS")
+elif(X!=25):
+    print("ORDINARY")
