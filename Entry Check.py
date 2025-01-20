b660f5bf5c8f4efae792e75fdb979920212de867
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 16:58:07 +0530

    Code for the Problem Entry Check
    This is the Code for the Problem: Entry Check

---
+++ Entry Check.py
@@ -0,0 +1,12 @@
+- Link: https://www.codechef.com/problems/P1169 
+- Contest: Starters 169
+- Difficulty: 177
+
+Code:
+X=int(input())
+if(X>10):
+    print("YES")
+elif(X==10):
+    print("YES")
+elif(X<10):
+    print("NO")
