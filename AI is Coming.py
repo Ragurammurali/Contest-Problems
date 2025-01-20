c6b59362013086c7d180b3d30f6b0eeebaf52329
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 16:56:14 +0530

    Code for the Problem AI is Coming
    This is the Code for the Problem: AI is Coming

---
+++ AI is Coming.py
@@ -0,0 +1,12 @@
+- Link: https://www.codechef.com/problems/AICOM 
+- Contest: Starters 153
+- Difficulty: 167
+
+Code:
+X=int(input())
+if(X>60):
+    print("NO")
+elif(X<60):
+    print("YES")
+elif(X==60):
+    print("YES")
